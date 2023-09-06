c5c95bcf52ed0393b277f2878bce7f8dbd3f7cdf
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-06 13:08:28 +0000

    copied program

--- .ccls-cache/@home@runner@Ch11Q1P15-using-ptrs-to-find-the-length-of-a-string/main.c
+++ .ccls-cache/@home@runner@Ch11Q1P15-using-ptrs-to-find-the-length-of-a-string/main.c
@@ -1,6 +1,24 @@
+// Function to count the characters in a string - Pointer version
+
 #include <stdio.h>
 
+// Decalre Functions
+int stringLength (const char *);
+
 int main(void) {
-  printf("Hello World\n");
+
+  printf ("%d  ", stringLength("stringLength test"));
+  printf ("%d  ", stringLength (""));
+  printf ("%d\n", stringLength ("complete"));
+  
   return 0;
+}
+
+// Define Functions
+int stringLength (const char *string) {
+  const char *cptr = string;
+  while (*cptr)
+    ++cptr;
+
+  return cptr - string;
 }
